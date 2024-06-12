ff1fb58ee30ecefd0eec148e743c40085c24f4dd
Hasan Taha Künkül <tahaknkl2323@gmail.com>
2024-06-12 17:04:09 +0300

    chore: Update dependencies to latest versions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:binance_app/screens/home/home.screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Binance App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: const HomeScreen(),
     );
   }
 }
